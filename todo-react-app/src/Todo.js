fba05991346208d50a6300b0432ffbdbe30abdf9
witwint <witwint1234@gmail.com>
2023-01-02 18:39:50 +0900

    test 컴포넌트추가

---
+++ todo-react-app/src/Todo.js
@@ -0,0 +1,14 @@
+import { ClassNames } from '@emotion/react';
+import React from 'react';
+
+const Todo = () => {
+  return (
+    <div className="Todo">
+      <input type= "checkbox" id="todo0" name="todo8" value="todo0" />
+      <label for="todo0"> Todo 컴포넌트 만들기 </label>
+    </div>
+
+  )
+}
+
+export default Todo;
