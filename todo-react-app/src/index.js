bde31db49083174d9218b707332204d1a61caee8
witwint <witwint1234@gmail.com>
2023-01-05 18:50:45 +0900

    ReactDOM > AppRouter 로 인덱스js 변경 오타수정

--- todo-react-app/src/index.js
+++ todo-react-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createRoot} from 'react-dom/client'
 import AppRouter from './AppRouter';
 
 const container = document.getElementById('root');
