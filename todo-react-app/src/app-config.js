4d5982628f23c878f0d727e5b7b38c31b354c3ad
witwint <witwint1234@gmail.com>
2023-01-10 22:24:41 +0900

    프론트 배포 설정

--- todo-react-app/src/app-config.js
+++ todo-react-app/src/app-config.js
@@ -5,7 +5,7 @@ const hostname = window && window.location && window.location.hostname;
 if (hostname === "localhost") {
   backendHost = "http://localhost:8080";
 } else {
-  backendHost = "http://prod-todo-api-service-witwint.us-west-2.elasticbeanstalk.com";
+  backendHost = "http://prod-todo-api-service-witwint.ap-northeast-2.elasticbeanstalk.com";
 }
 
 export const API_BASE_URL = `${backendHost}`;
